8e56980d61f1288ec17e0dade2f2447376cc6b52
Sayed Raianul Kabir <raianul.berlin@gmail.io>
2026-02-21 21:47:14 +0100

    change forntend to app

--- app/next.config.js
+++ app/next.config.js
@@ -1,8 +1,14 @@
+const path = require("path");
+
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   output: "standalone",
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
+  },
+  webpack: (config, { isServer }) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
   },
 };
